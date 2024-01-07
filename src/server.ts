b201a0a37d4854a815ed5a08a7ba1553f8922426
Antônio Sales <antoniosales1003@gmail.com>
2024-01-07 09:01:16 -0300

    feat: prehandler global for logging

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+// global preHandler
+app.addHook('preHandler', async (request) => { console.log(`[${request.method}] ${request.url}`) })
+
+
 app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
